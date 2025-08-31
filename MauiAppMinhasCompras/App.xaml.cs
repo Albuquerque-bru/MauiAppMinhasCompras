5d446f4a290329fa57e5b916c09aee26c0e1c108
bruna albuquerque <albuquerque.rb@hotmail.com>
2025-08-31 19:50:15 -0300

    teste recombilação

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        static SQLiteDataBaseHelper _db; //campo
+
+        public static SQLiteDataBaseHelper Db //propriedade
+        {
+            get 
+            {
+                if(_db == null) 
+                {
+                    string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDataBaseHelper(path);
+                }
+
+                return _db; 
+            }
+        }
         public App()
         {
             InitializeComponent();
